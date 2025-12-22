415b064014bc2a7454d73becde2fcbf30e929e13
Cember <cyingg0000@gmail.com>
2025-12-22 13:57:41 +0800

    Update portfolio

--- js/script.js
+++ js/script.js
@@ -1,25 +1,25 @@
-// Navigation Bar 
+// Navigation Bar
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
 menu.onclick = () => {
-    menu.classList.toggle('bx-x');  
-    navbar.classList.toggle('open'); 
+	menu.classList.toggle('bx-x');
+	navbar.classList.toggle('open');
 };
 
 // Navigation Menu Button is only visible when screen width is smaller
-window.addEventListener('resize', function() {
-    if (window.innerWidth > 800) {
-        navbar.classList.remove('open');  
-        menu.classList.remove('bx-x'); 
-    }
+window.addEventListener('resize', function () {
+	if (window.innerWidth > 800) {
+		navbar.classList.remove('open');
+		menu.classList.remove('bx-x');
+	}
 });
 
 // ScrollReveal
 const sr = ScrollReveal({
-    distance: '40px',
-    duration: 2500,
-    reset: true
+	distance: '40px',
+	duration: 2500,
+	reset: true,
 });
 
 sr.reveal('.logo', { delay: 200, origin: 'left' });
